122e56e33b604a00dc95a25dac11d7ccb15a7c35
josejonasalvesmorais <codewjonas@gmail.com>
2026-03-14 15:37:17 -0300

    Nova atualização

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = 'Jonas'
+    idade = 22
+
+    context = {
+        'usuario': usuario,
+        'idade': idade
+        }
+    return render_template('index.html', context=context)
+
+@app.route('/nova/')
+def novapag():
+    return "Outra view"
